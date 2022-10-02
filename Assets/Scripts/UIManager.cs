7fb129f0bec64d910dce332facc3ce51384099b3
Esben Nyboe <enj@kanda.dk>
2022-10-02 18:08:26 +0200

    taking damage

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -13,6 +13,12 @@ public class UIManager : MonoBehaviour
     [FormerlySerializedAs("_timeText")] [SerializeField] Text timeText;
 
     private float _score;
+    private float _startTime;
+    private float _timer;
+    [SerializeField] private float timerFactor;
+    [SerializeField] private int lives = 10;
+
+    private bool _gameOver;
 
     private void Awake()
     {
@@ -33,21 +39,34 @@ public class UIManager : MonoBehaviour
     {
         scoreText.text = "Score: " + 0 + " pts";
         timeText.text = "Time: " + 0 + " sec";
+        _timer = 0;
     }
 
     void Update()
     {
-        float timeCalc = Time.fixedTime;
+        _timer += Time.deltaTime;
+        float timeCalc = _timer * timerFactor;
 
-        // float scoreCalc = _score / timeCalc;
-        float scoreCalc = _score;
+        float scoreCalc = _score / timeCalc;
+        // float scoreCalc = _score;
 
-        scoreText.text = "Score: " + (int)scoreCalc + " pts";
-        timeText.text = "Time: " + (int)timeCalc + " sec";
+        scoreText.text = "Score: " + (int)scoreCalc;// + " pts";
+        // timeText.text = "Time: " + (int)_timer;// + " sec";
+        timeText.text = "Lives: " + lives;// + " sec";
     }
 
     public void AddToScore(int points)
     {
         _score += points;
+    }
+
+    public void TakeDamage(int points)
+    {
+        lives -= points;
+        if (lives < 1)
+        {
+            // PLAY SOUND: GAME OVER
+            _gameOver = true;
+        }
     }
 }
