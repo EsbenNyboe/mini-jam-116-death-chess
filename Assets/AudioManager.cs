ac5b6a6b32c0fbc2c1004123b41de67307759149
Esben Nyboe <enj@kanda.dk>
2022-10-21 18:19:43 +0200

    add failed audiomanager-script

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // public static AudioManager Instance;
+    // private void Awake()
+    // {
+    //     if (Instance)
+    //     {
+    //         if (Instance != this)
+    //         {
+    //             Destroy(gameObject);
+    //         }
+    //     }
+    //     else
+    //     {
+    //         Instance = this;
+    //         DontDestroyOnLoad(gameObject);
+    //     }
+    // }
+}
